85480478908ef2acc41f42c1fc475b8cc67fe732
Alyssa Winn <alyssawinn2@hotmail.com>
2021-05-23 19:56:41 -0600

    friend routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -31,6 +31,22 @@ const userController = {
         .then(dbUserData => res.json(dbUserData))
         .catch(err => res.status(400).json(err));
     },
+    // POST /api/users/:userId/friends/:friendId
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $addToSet: { friends: params.friendId }},
+            { new: true, runValidators: true }
+        )
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this user id'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
     // PUT /api/users/:id
     updateUser({ params, body }, res) {
         User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
@@ -52,6 +68,22 @@ const userController = {
                 return;
             }
             res.json({ message: 'User deleted' });
+        })
+        .catch(err => res.status(400).json(err));
+    },
+    // DELETE /api/users/:userId/friends/:friendId
+    deleteFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId }},
+            { new: true, runValidators: true}
+        )
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
         })
         .catch(err => res.status(400).json(err));
     }
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,8 +5,11 @@ const {
     getUserById,
     createUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    addFriend,
+    deleteFriend
 } = require('../../controllers/user-controller');
+const { add } = require('../../models/Reaction');
 
 router
     .route('/')
@@ -18,5 +21,10 @@ router
     .get(getUserById)
     .put(updateUser)
     .delete(deleteUser)
+
+router
+    .route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend)
 
 module.exports = router;
